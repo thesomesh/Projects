2a77ed95b51ebb53ad8ffbe40889756fab7f128d
thesomesh <91958243+thesomesh@users.noreply.github.com>
2025-07-16 17:20:04 +0530

    Delete Uptime/services directory

--- Uptime/services/WebsiteStatusService.java
+++
@@ -1,9 +0,0 @@
-package com.app.service;
-
-import java.util.List;
-
-import com.app.dto.WebsiteStatusDto;
-
-public interface WebsiteStatusService {
-public List<WebsiteStatusDto> getWebsiteStatus(Long id);
-}
